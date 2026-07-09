03dcef4e0974cb4611346c938601366b4a34b41f
dewangga-crypto <dewangga.julio@gmail.com>
2026-07-10 07:47:07 +0800

    Update staff.py

--- cogs/staff.py
+++ cogs/staff.py
@@ -5,8 +5,7 @@ Moderation and economy management commands
 """
 
 import discord
-from discord.ext import commands
-import app_commands
+from discord.ext import commands, app_commands
 import logging
 
 logger = logging.getLogger('CasinoForge.Staff')
